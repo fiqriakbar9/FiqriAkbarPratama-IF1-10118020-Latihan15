928c97e6846fe83ec722a2f08cc3ae737aad7e99
fiqriakbar9 <42399322+fiqriakbar9@users.noreply.github.com>
2019-09-24 21:06:31 +0700

    Add files via upload

---
+++ src/logika/Logika.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logika;
+
+/**
+ *
+ * Nama : Fiqri Akbar Pratama
+ * NIM  : 10118020  
+ * Kelas: IF-1
+ * Deskripsi Program : Program ini berisi program untuk menampilkan 
+ * logika sederhana
+ * 
+ */
+
+public class Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int umur = 19;
+        boolean dewasa;
+        dewasa = ( umur >= 17);
+        System.out.println("Status dewasa : " + dewasa);
+    }
+    
+}
